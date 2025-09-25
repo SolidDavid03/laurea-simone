c1fd28ae027f5addd9ea9290c1c4915e0db09896
SolidDavid03 <ficocellidavide4@gmail.com>
2025-09-25 16:42:40 +0200

    Aggiunta riproduzione video auto

---
+++ src/script.js
@@ -0,0 +1,21 @@
+function playVideo(videoEl) {
+  videoEl.muted = true;
+  videoEl.play();
+  videoEl.muted = false;
+}
+
+const videoInvito = document.getElementById("video-invito");
+
+const obsCallback = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  if (entry.isIntersecting && videoInvito.currentTime === 0)
+    playVideo(videoInvito);
+};
+const obsOptions = {
+  root: null,
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(videoInvito);
