c0eb9e0baff193783009d1365f2fd3f500d6d032
SolidDavid03 <ficocellidavide4@gmail.com>
2025-10-15 18:33:31 +0200

    Crediti (#6)
    * Aggiunta sezione crediti e fixato layout barra di navigazione
    
    * Aggiunto orario della proclamazione
    
    * Aggiunto link linkedin per Davide
    
    * Modifiche varie
    
    * Aggiunto pulsante per contatto

--- src/script.js
+++ src/script.js
@@ -35,3 +35,21 @@ dropdownButtons.forEach((dropdownBtn) => {
     svgIcon.classList.toggle("rotate-180");
   });
 });
+
+const dropdownArrow = document.getElementById("dropdown-arrow");
+const menu = document.getElementById("menu");
+
+function toggleMenu() {
+  menu.classList.toggle("hidden");
+  const isMenuEnabled = !Array.from(menu.classList).includes("hidden");
+
+  if (!isMenuEnabled) {
+    dropdownArrow.classList.remove("dropdown-arrow-menu-enabled");
+    dropdownArrow.classList.add("dropdown-arrow-menu-disabled");
+  } else {
+    dropdownArrow.classList.remove("dropdown-arrow-menu-disabled");
+    dropdownArrow.classList.add("dropdown-arrow-menu-enabled");
+  }
+}
+
+dropdownArrow.addEventListener("click", toggleMenu);
